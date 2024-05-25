ee9a406eb2779e9ee8846fb730335ccdeb850d21
Lucas Matheus da Silva Peres <83469680+LuscaMath@users.noreply.github.com>
2024-05-25 17:59:21 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 const menu_hamburger = document.getElementById('menu-hamburger')
 const mobile_menu = document.getElementById('navigation')
+const root = document.querySelector(':root')
+const body = document.querySelector('body')
+const toggle = document.querySelector('#theme-switch')
+
+toggle.addEventListener('click', ()=> {
+    if (body.dataset.theme === 'light') {
+        root.style.setProperty('--bg-color', '#000000')
+        root.style.setProperty('--a-color', '#ffffff')
+        root.style.setProperty('--p-d-color', '#ffffff')
+        root.style.setProperty('--main-collor', '#2b4360')
+        root.style.setProperty('--sub-title-color', '#cccccc')
+        root.style.setProperty('--title-color', '#cccccc')
+        body.dataset.theme = 'dark'
+    } else {
+        root.style.setProperty('--bg-color', '#ffffff')
+        root.style.setProperty('--a-color', '#000000')
+        root.style.setProperty('--p-d-color', '#000000')
+        root.style.setProperty('--main-collor', '#2994D1')
+        root.style.setProperty('--sub-title-color', '#2994D1')
+        root.style.setProperty('--title-color', '#cccccc')
+        body.dataset.theme = 'light'
+    }
+})
 
 menu_hamburger.addEventListener('click', () => {
     if (menu_hamburger.classList.contains('hamburger')) {
